1cff129d66d58621619cee3beb437508c7ec2e06
architarious <coletrainwvu@gmail.com>
2015-03-03 19:03:51 -0500

    styled modular footer for mobile

--- javascripts/page/default.js
+++ javascripts/page/default.js
@@ -3,7 +3,7 @@ require(['jquery', 'bootstrap/collapse', 'bootstrap/carousel', 'bootstrap/dropdo
 	$('.site-logo').text('[changed by jQuery]');
 
 
-	
+		
 
 	$('#mainCarousel').carousel({
 		interval:false,
@@ -23,30 +23,54 @@ require(['jquery', 'bootstrap/collapse', 'bootstrap/carousel', 'bootstrap/dropdo
     	autoCoefficient: 6
     });
 
-
+   
     
-	$('#postfix--thumbs').perfectScrollbar({
-		suppressScrollY: true,
-		minScrollBarLength: 167
-
-	});
-    
-    overthrow.sidescroller(
-      document.querySelectorAll( ".overthrow-enabled .sidescroll-nextprev" ), {disableNav: true, fixedItemWidth: true, snapScroll: true });
-
-    overthrow.sidescroller( document.querySelectorAll(".sidescroll"), {
-        snapScroll: true,
-        snapTolerance: 30
-    });
-
-    overthrow.sidescroller( document.querySelectorAll( ".overthrow-enabled .snapscroll" ), { snapScroll: false });
-
-
-    
-    resizeSegmentTitles("h1.title");
+	resizeSegmentTitles("h1.title");
    	$('.collapse').collapse(); //bootstrap solution for navigation "navbar"$('.site-logo').bigText($('.site-logo'));
 
-   	
+   	$(function(){ 
+   	  
+   	  var month = [];
+   	  month[0] = "Jan";
+   	  month[1] = "Feb";
+   	  month[2] = "Mar";
+   	  month[3] = "Apr";
+   	  month[4] = "May";
+   	  month[5] = "Jun";
+   	  month[6] = "Jul";
+   	  month[7] = "Aug";
+   	  month[8] = "Sep";
+   	  month[9] = "Oct";
+   	  month[10] = "Nov";
+   	  month[11] = "Dec";
+
+   	   ev = {}
+
+   	   $('.b-wvuCalendar .eventName').each(function(i, event) {
+   	    
+   	     ev.title = $(event).find('a').text();
+   	     ev.link = $(event).find('a').attr('href');
+
+   	   });//end .eventName
+
+   	  $('.b-wvuCalendar .eventDate').each(function(i, event) {
+   	   
+   	    ev.date = new Date($(this).text());
+   	    ev.month = month[ev.date.getMonth()];
+   	    ev.day = ev.date.getDate();
+
+   	    $(this).html('<a href="' + ev.link + '" class="b-wvuCalendar__event"><span class="b-wvuCalendar__event_month">' + ev.month + '</span><span class="b-wvuCalendar__event_day">' + ev.day + '</span></a>')
+
+   	    
+
+
+   	  });//end .eventDate
+
+   	  
+   	  
+   	});//end calendar script
+
+   
 
     
     /* these former utilities have been replaced by addthis
@@ -72,5 +96,21 @@ require(['jquery', 'bootstrap/collapse', 'bootstrap/carousel', 'bootstrap/dropdo
 	   due to it's ability to target individual links
 	   smoothScroll();
 	*/
+
+	$('#postfix--thumbs').perfectScrollbar({
+		suppressScrollY: true,
+		minScrollBarLength: 167
+
+	});
+    
+    overthrow.sidescroller(
+      document.querySelectorAll( ".overthrow-enabled .sidescroll-nextprev" ), {disableNav: true, fixedItemWidth: true, snapScroll: true });
+
+    overthrow.sidescroller( document.querySelectorAll(".sidescroll"), {
+        snapScroll: true,
+        snapTolerance: 30
+    });
+
+    overthrow.sidescroller( document.querySelectorAll( ".overthrow-enabled .snapscroll" ), { snapScroll: false });
 
 });
